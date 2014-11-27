33be2f32e1674196486eaa2c4202446e54284d27
Ahmadreza <ara.mailbox@gmail.com>
2014-11-27 22:47:46 +1100

    send mail via codolutions production

--- js/mailer.js
+++ js/mailer.js
@@ -2,9 +2,7 @@ var orangePhotography = orangePhotography || {};
 
 (function  ($) {
 	orangePhotography.mailer = (function  () {
-		//var url = 'https://www.codolutions.com/mailermodule/send';
-		//var url = 'https://codolutionsdev.azurewebsites.net/mailermodule/send';
-		var url = 'http://localhost:3000/mailermodule/send';
+		var url = 'https://www.codolutions.com/mailermodule/send';
 		var sendfun = function  (subject, html, done, error) {
 			var data = {
 				subject:subject,
@@ -29,17 +27,40 @@ var orangePhotography = orangePhotography || {};
 	$(document).ready(function  () {
 
 		$("#contact-form").submit(function  () {
+			block();
 			orangePhotography.mailer.send($('#subject').val(), 
 				'A message from:' + $('#name').val() + '&lt;' + $('#email').val() + '&gt;<br/>' +  $('#message').val(),
 				function  () {
+					unblock();
 					console.log('done');
 				},
 				function  () {
+					unblock();
 					console.log('error');
 				})
 			;
 			event.preventDefault();
 		});
 	});
+
+    var blockUI = document.createElement("div");
+    blockUI.setAttribute("id", "blocker");
+    blockUI.innerHTML = '<div>Sending...<img src="http://www.socialups.com/static/images/fbinventory/ajax_loader.gif"></div>';
+    document.body.appendChild(blockUI);
+    
+    
+    var cover = document.getElementById("blocker").style.display = "none";
+    
+    var btn = document.getElementById("bloc");
+    
+    btn.onclick = block;
+    
+    function block()
+    {
+        document.getElementById("blocker").style.display = "";
+    }
+    function unblock(){
+        document.getElementById("blocker").style.display = "none";
+    }
 
 })(jQuery);
